844db31eaa16ded563160891ae1fb7c0e7a422d2
thayate <e36469@jp.ibm.com>
2019-09-27 16:37:21 +0900

    Add files via upload

---
+++ Enhanced-Forecast/v3_FoDPull.py
@@ -0,0 +1,25 @@
+import requests, json, csv, math
+# from urllib.request import urlopen
+import datetime
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+
+#緯度経度の定義
+lat = '35.495'
+lon = '138.991'
+
+#API Keyの定義
+apiKey = ''
+
+url = 'https://api.weather.com/v3/wx/forecast/hourly/15day?geocode=' + lat + "," + lon
+payload = {'units': 'm', 'format': 'json', 'language': 'ja-JP'}
+payload['apiKey'] = apiKey
+
+data_dict = requests.get(url, params=payload).json()
+data_str = json.dumps(data_dict)
+df = pd.read_json(data_str)
+
+df['validTimeLocal'] = pd.to_datetime(df['validTimeLocal'])
+df['validTimeLocal'] = df['validTimeLocal'] + datetime.timedelta(hours=9) #JSTに変換（+9時間)
+
+df.to_csv("filename.csv", index = False) #csvに出力
